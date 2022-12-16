54395daa6d9a89dcdee11e16a85f39c5a4f3e99a
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-15 21:10:35 -0300

    feat: findById and save methods (not implemented)

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -9,11 +9,19 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   // Apenas os repositórios da camada do prisma vão ter acesso ao banco!
   constructor(private prismaService: PrismaService) {}
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
