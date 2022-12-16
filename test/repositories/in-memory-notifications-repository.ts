32352dbce4073c62bcef8e4ebe0df1a3eb36defb
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-15 21:11:13 -0300

    test: Create findyById and save unit tests

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,32 @@ export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      // O find retorna undefined quando não encontra nada, por preferência de trabalhar com null nós retornamos assim
+      return null;
+    }
+
+    return notification;
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    // Se a notificação existir, vamos sobrepor com a notificação que queremos salvar
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 }
