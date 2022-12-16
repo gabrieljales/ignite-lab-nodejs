2830bb164be7b45924ddda802e9dee06e01eb3da
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-15 22:25:13 -0300

    feat: Create countManyByRecipientId method

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -4,4 +4,5 @@ export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract findById(notificationId: string): Promise<Notification | null>; // null é caso a notificação não exista
   abstract save(notification: Notification): Promise<void>;
+  abstract countManyByRecipientId(recipientId: string): Promise<number>;
 }
