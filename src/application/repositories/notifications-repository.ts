1b4434d9fcc52e8ded0957e049b797cea064fc80
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-15 21:09:31 -0300

    feat: findById and save methods created

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../entities/Notification';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>; // null é caso a notificação não exista
+  abstract save(notification: Notification): Promise<void>;
 }
