42303a3844ed914619b9fc00c56fc685cb4bd581
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-15 21:07:30 -0300

    feat: Config jest moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-export default {
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  // rootDir: 'src', // Removido para ler arquivos fora da pasta src
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
@@ -8,4 +12,9 @@ export default {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
+
+export default config;
