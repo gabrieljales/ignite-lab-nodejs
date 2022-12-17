50c4abcbbf373e910234bbf40bdeb88e87c03cce
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-17 15:36:59 -0300

    feat: Create read-notification use-case and spec file

---
+++ src/application/use-cases/read-notification.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface IReadNotificationRequest {
+  notificationId: string;
+}
+
+// Não da pra fazer isso com interface
+type IReadNotificationResponse = void;
+
+@Injectable()
+export class ReadNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: IReadNotificationRequest,
+  ): Promise<IReadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationsRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.read();
+
+    await this.notificationsRepository.save(notification);
+  }
+}
