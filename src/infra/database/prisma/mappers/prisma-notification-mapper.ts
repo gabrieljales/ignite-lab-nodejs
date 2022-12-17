f0342149ba30ed5a1459ddc0b23f952939ef706b
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-12-17 16:01:29 -0300

    feat: Create toDomain mapper
    Prisma entity -> Domain entity

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/Notification';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   // Método static para não ser necessário instanciar a classe
@@ -11,5 +13,20 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  // Converte Notification do prisma para a entidade da camada de domínio da aplicação
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt, // Nesse caso é uma notificação já existente que queremos salvar no banco, por isso o campo preenchido
+      },
+      raw.id, // Id que já existe no banco de dados, NÃO é uma nova notificação
+    );
   }
 }
